dddef96c3c0fcafa5debfe058cc31660fae36ed0
OLoKo64 <naldoepingo1@gmail.com>
2022-05-14 12:56:22 -0300

    feat: move class to another file

---
+++ classes/classes.py
@@ -0,0 +1,8 @@
+class Game:
+    def __init__(self, name='', discounted_price='', link='', original_price='', date='', discount=''):
+        self.name = name
+        self.discounted_price = discounted_price
+        self.link = link
+        self.original_price = original_price
+        self.date = date
+        self.discount = discount
